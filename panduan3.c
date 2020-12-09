b4c74bba588e17b0fba34b0782a81aa7be2f040f
song_biao_123 <759403720@qq.com>
2020-12-09 23:50:09 +0800

    适当的使用{}可以使代码的逻辑更加清楚，代码风格很重要

---
+++ panduan3.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//适当的使用{}可以使代码的逻辑更加清楚
+//代码风格很重要
+int main(){
+	int a = 1;
+	int b = 2;
+	if (a == 1)
+	{
+		if (b == 2)
+		{
+			printf("hehe\n");
+		}
+	}
+	else
+	{
+		printf("haha\n");
+	}
+	return 0;
+}
